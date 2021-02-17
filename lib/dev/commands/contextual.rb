1141cd8aa7c3452bc4971fcd82222f89ea4a0a3a
Burke Libbey <burke@libbey.me>
2021-02-17 18:07:42 -0500

    subcommands

--- lib/dev/commands/contextual.rb
+++ lib/dev/commands/contextual.rb
@@ -8,22 +8,29 @@ module Dev
       def call(args, name)
         cfg = Project.current.config
 
-        script = if DEFAULT_COMMANDS.include?(name) && cfg.key?(name)
-          configstr_run(cfg[name])
+        sh, args = if DEFAULT_COMMANDS.include?(name) && cfg.key?(name)
+          configstr_run(cfg[name], args)
         else
-          configstr_run(cfg['commands'][name])
+          configstr_run(cfg['commands'][name], args)
         end
 
-        puts("\x1b[1;34mexecuting #{script}\x1b[0m")
-        exec('/bin/bash', '-c', script)
+        puts("\x1b[1;34mexecuting #{sh}\x1b[0m")
+
+        sh.concat(' "$@"') if sh.lines.size == 1 && sh !~ /\$[@\*]/
+        sh = %Q{runcmd() {\n#{sh}\n}\nruncmd "$@"}
+        exec('/bin/bash', '-c', sh, '--', *args)
       end
 
-      def configstr_run(x)
+      def configstr_run(x, args)
         case x
         when Hash
-          x['run']
+          if x['subcommands']&.include?(args[0])
+            [x['subcommands'][args.shift]['run'], args]
+          else
+            [x['run'], args]
+          end
         when String
-          x
+          [x, args]
         else
           raise(Abort, "idk")
         end
