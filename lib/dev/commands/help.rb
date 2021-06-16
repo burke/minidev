350c6a74dd07f48f472f15295c416066e47ad29b
Misty De Méo <misty.demeo@shopify.com>
2021-06-16 12:45:19 -0700

    Help: skip helpless commands

--- lib/dev/commands/help.rb
+++ lib/dev/commands/help.rb
@@ -10,6 +10,7 @@ module Dev
         Dev::Commands::Registry.resolved_commands.each do |name, klass|
           next if name == 'help'
           puts CLI::UI.fmt("{{command:#{Dev::TOOL_NAME} #{name}}}")
+          next unless klass.respond_to?(:help)
           if help = klass.help
             puts CLI::UI.fmt(help)
           end
