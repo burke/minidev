a16c6eb7b582bc0196f223131dbf51f80323909f
Tobias Lütke <tobi@shopify.com>
2024-07-02 10:29:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 mkdir -p $HOME/.local
 git clone https://github.com/burke/minidev.git $HOME/.local/minidev
-echo ". $HOME/.local/minidev/dev.sh" >> $HOME/.profile
+
+echo "source $HOME/.local/minidev/dev.sh" >> $HOME/.bashrc
+echo "source $HOME/.local/minidev/dev.sh" >> $HOME/.zshrc
+
 
 # if you are in a devcontainer and it follows the /workspaces convention let's set that as your goto
 if [ -d /workspaces ]; then
